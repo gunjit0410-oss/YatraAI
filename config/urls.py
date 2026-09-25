3dcc6f7d331859d1b85df421fa70ed1092453b9e
Gunjit verma <gunjit0410@gmail.com>
2026-09-25 19:22:44 +0530

    Fix favicon 404 errors and add WhiteNoise static file handler

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/logo.png', permanent=True)),
+    path('favicon.png', RedirectView.as_view(url='/static/images/logo.png', permanent=True)),
     path('', include('tourism.urls')),
 ]
+
